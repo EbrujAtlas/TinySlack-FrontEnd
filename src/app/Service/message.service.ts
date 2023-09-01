c327098c76e422c1c9560329d1a3631f9508563f
Ebru Jolivet <ebruozogul1@gmail.com>
2023-09-01 13:40:50 +0200

    creation des messages

--- src/app/Service/message.service.ts
+++ src/app/Service/message.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
+  url = 'http://localhost:8080/tinyslack/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMessages() {
+    return this.http.get(this.url + 'messages');
+  }
+
+  getMessageById(id: number) {
+    return this.http.get(this.url + 'messages/' + id);
+  }
 }
--- src/app/component/message/message.component.ts
+++ src/app/component/message/message.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Messages } from 'src/app/Model/messages';
+import { MessageService } from 'src/app/Service/message.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.css'],
 })
 export class MessageComponent {
+  message: Messages = {
+    messageId: '00000',
+    message: 'default',
+    messageDate: new Date(2000, 10, 10),
+    channelId: '1111',
+    userId: '010101',
+  };
 
+  constructor(private ar: ActivatedRoute, private msg: MessageService) {
+    let id = this.ar.snapshot.params['id'];
+    console.log(id);
+    if (id != undefined) {
+      this.msg.getMessageById(id).subscribe((data: any) => {
+        this.message = data;
+      });
+    }
+  }
 }
