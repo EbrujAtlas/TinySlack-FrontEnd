c91f5daa70a3eab74189e0134976356af6619ef9
Ebru Jolivet <ebruozogul1@gmail.com>
2023-09-01 09:33:15 +0200

    creation architecture message et canal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelComponent } from './component/channel/channel.component';
+import { ListChannelComponent } from './component/list-channel/list-channel.component';
+import { CreateChannelComponent } from './component/create-channel/create-channel.component';
+import { MessageComponent } from './component/message/message.component';
+import { CreateMessageComponent } from './component/create-message/create-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelComponent,
+    ListChannelComponent,
+    CreateChannelComponent,
+    MessageComponent,
+    CreateMessageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/list-channel/list-channel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-channel',
+  templateUrl: './list-channel.component.html',
+  styleUrls: ['./list-channel.component.css']
+})
+export class ListChannelComponent {
+
+}
