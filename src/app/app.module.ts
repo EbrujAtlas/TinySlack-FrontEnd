adf8a528feed4e63a1ab84a4404f10ce609c919f
Ebru Jolivet <ebruozogul1@gmail.com>
2023-09-02 18:48:14 +0200

    all conflict fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { ChannelComponent } from './component/channel/channel.component';
 import { ListChannelComponent } from './component/list-channel/list-channel.component';
 import { CreateChannelComponent } from './component/create-channel/create-channel.component';
 import { MessageComponent } from './component/message/message.component';
 import { CreateMessageComponent } from './component/create-message/create-message.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-=======
+
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { ProfileComponent } from './component/profile/profile.component';
 import { Error404Component } from './component/error404/error404.component';
 import { AboutComponent } from './component/about/about.component';
->>>>>>> origin/user
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+
     ChannelComponent,
     ListChannelComponent,
     CreateChannelComponent,
     MessageComponent,
     CreateMessageComponent,
-=======
+
     LoginComponent,
     SignupComponent,
     ProfileComponent,
     Error404Component,
-    AboutComponent
+    AboutComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
->>>>>>> origin/user
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
